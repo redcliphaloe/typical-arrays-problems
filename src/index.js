3d51c87e720376195fc27aa927f75fdb90fbe388
redcliphaloe <redcliphaloe@gmail.com>
2020-03-16 17:00:15 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array) {
+        return 0;
+    }
+    let result = 0;
+    array.forEach(element => {
+        if (isNaN(element)) {
+            return 0;
+        }
+        element < result ? result = element : null;
+    });
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array) {
+        return 0;
+    }
+    let result = 0;
+    array.forEach(element => {
+        if (isNaN(element)) {
+            return 0;
+        }        
+        element > result ? result = element : null;
+    });
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!(array && array.length)) {
+        return 0;
+    }
+    let sum = 0;
+    array.forEach(element => {
+        if (isNaN(element)) {
+            return 0;
+        }        
+        sum += element;
+    });
+  return sum / array.length;
 }
